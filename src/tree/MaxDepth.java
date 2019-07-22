7b3c2aa5a03f9d7cd3b3ec2a83e4aa55630db668
xinze <1050748528@qq.com>
2019-07-22 10:06:54 +0800

    2019/7/22

---
+++ src/tree/MaxDepth.java
@@ -0,0 +1,12 @@
+package tree;
+
+public class MaxDepth {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = maxDepth(root.left) + 1;
+        int right = maxDepth(root.right) + 1;
+        return Math.max(left, right);
+    }
+
+}
