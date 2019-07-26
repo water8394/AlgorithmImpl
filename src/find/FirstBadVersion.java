eedaf6a0e0e4764133b75276b9253567fbb28cf8
xinze <1050748528@qq.com>
2019-07-26 16:34:02 +0800

    2019/7/26

---
+++ src/find/FirstBadVersion.java
@@ -0,0 +1,23 @@
+package find;
+
+public class FirstBadVersion {
+
+    public boolean isBadVersion(int version){
+        return true;
+    }
+
+    public int firstBadVersion(int n) {
+        int l = 1, r = n;
+
+        while (l < r){
+            int mid = l + (r-l) / 2;
+            if (isBadVersion(mid)){
+                r = mid -1;
+            }else{
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+
+}
