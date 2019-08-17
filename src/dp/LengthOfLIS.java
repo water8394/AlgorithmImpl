d3b54272e5dea428772867da4d5b879c661e3192
xinze <1050748528@qq.com>
2019-08-18 07:34:12 +0800

    2019/8/17

---
+++ src/dp/LengthOfLIS.java
@@ -0,0 +1,24 @@
+package dp;
+
+import java.util.Arrays;
+
+public class LengthOfLIS {
+    public int lengthOfLIS(int[] nums){
+
+        int n = nums.length;
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+
+            int max = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]){
+                    max = Math.max(dp[j]+1,max);
+                }
+            }
+            dp[i] = max;
+        }
+
+        return Arrays.stream(dp).max().orElse(0);
+
+    }
+}
